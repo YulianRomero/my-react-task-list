c8818dd0a5d769565a12ac30173e21e1a848db6d
yulian rodriguez <rodriguezyulian16@gmail.com>
2023-07-13 20:54:35 -0600

    implementando local storage

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,47 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Tasklist from "./Tasklist";
 
 const Task = () => {
-  
-  const [list, setList] = useState([]);
+  const local = JSON.parse(localStorage.getItem("tareas"));
+  const [list, setList] = useState(local || []);
+  const [description, setDescription] = useState("");
+  useEffect(() => {
+     if(local !== null){
+        setList(local);
+        }
+        else{localStorage.setItem("tareas", JSON.stringify(list));
+    }
+  }, []);
 
-    const handleAddItem = addItem => {
-        setList([...list, addItem]);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([
+      ...list,
+      {
+        id: Math.floor(Math.random() * 10 ** 3),
+        description,
+        done: false,
+        editable: false,
+      },
+    ]);
+    localStorage.setItem("tareas", JSON.stringify(list));
+    console.log("task", list);
+    setDescription("");
+  };
 
-    const [description, setDescription] = useState(""); 
-    const handleSubmit = e => {
-        e.preventDefault(); 
-        
-        handleAddItem({
-            id: Math.floor(Math.random()* (10**3)),
-            description,
-            done: false
-        });
-        setDescription(""); 
-    };
-    return (
+  return (
     <div className="tasks">
-        <form onSubmit={handleSubmit}>
-            <div>
-                <div className="newtask">
-                    <input
-                        type="text"
-                        className="text"
-                        value={description}
-                        onChange={e => setDescription(e.target.value)}
-                    />
-                    <button
-                        // disabled={description ? "" : "disabled"}
-                    >
-                        Add
-                    </button>
-                </div>
-            </div>
-        </form>
-    <Tasklist list={list} setList={setList} />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div className="newtask">
+            <input
+              type="text"
+              className="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <button disabled={description ? "" : "disabled"}>Add</button>
+          </div>
         </div>
-    );
+      </form>
+      <Tasklist list={list} setList={setList} />
+    </div>
+  );
 };
 
 export default Task;
--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,52 +1,99 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-const Tasklist = (props) => {
+const Tasklist = ({ list, setList }) => {
+  localStorage.setItem("tareas", JSON.stringify(list));
+ 
+  const [selectedItemId, setSelectedItemId] = useState(null);
 
-    const {list, setList} = props;
 
-    useEffect(()=>{
-      onClickRemoveItem();
-    }, [])
+  const onChangeStatus = (e) => {
+    const name = Number(e.target.name);
+    const check = e.target.checked;
+    const updateList = list.map((item) => ({
+      ...item,
+      done: item.id === name ? check : item.done,
+    }));
+    setList(updateList);
+  };
 
-    const onChangeStatus = e => {
-        const name = Number(e.target.name);
-        const check = e.target.checked;
-        const updateList = list.map(item => ({
-            ...item,
-            done: item.id === name ? check : item.done
-        }));
-        setList(updateList);
-    };
+  const onHandleDelete = (e) => {
+    const tasklist = list.filter((item) => item.id !== e);
+    setList(tasklist);
+  };
 
-    
-    const onClickRemoveItem = e => {
-        const updateList = list.filter(item => !item.done);
-        setList(updateList);
-    };
+  const taskUpdateSelect = (e, datos) => {
+    const task = list.map((item) => ({
+      ...item,
+      editable: item.id === e ? true : item.editable,
+    }));
+    setList(task);
+    setSelectedItemId(datos);
+  };
 
-    return (
-        <div className="tasks">
-            
-        {list.length ? list.map(item => (
-           <div className="task" key={item.id}>
-           <input
-                   className="todo__state"
-                   name={item.id}
-                   type="checkbox"
-                   defaultChecked={item.done}
-                   onChange={onChangeStatus}
-                   id="miCheckbox"
-                   key={item.id}
-               />
-           <label className="todo new-item" for="miCheckbox">
-               {item.description}
-           </label>
-           <img src="./../src/images/update.png" alt="actualizar tareas" />
-           <img src="./../src/images/delete.png" alt="Eliminar tareas" onClick={onClickRemoveItem} />
+  const handleUpdate = (e, datos) => {
+    const task2 = list.map((item) => ({
+      ...item,
+      description: item.id === e ? datos : item.description,
+      editable: false,
+    }));
+    setList(task2);
+  };
+
+  const handleInputChange = (event) => {
+    setSelectedItemId(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <div className="tasks">
+      {list.length
+        ? list.map((item) => (
+            <div className="task" key={item.id}>
+              <input
+                className="todo__state"
+                name={item.id}
+                type="checkbox"
+                defaultChecked={item.done}
+                onChange={onChangeStatus}
+                id="miCheckbox"
+                key={item.id}
+              />
+              {item.editable ? (
+                <>
+                  <input
+                    type="text"
+                    id={item.id}
+                    value={selectedItemId}
+                    onChange={handleInputChange}
+                  />
+                  <img
+                    src="./../src/images/ok.png"
+                    alt="actualizar tareas"
+                    onClick={() => handleUpdate(item.id, selectedItemId)}
+                  />
+                </>
+              ) : (
+                <>
+                  <label className="todo new-item" htmlFor="miCheckbox">
+                    {item.description}
+                  </label>
+                  <img
+                    src="./../src/images/update.png"
+                    alt="actualizar tareas"
+                    onClick={() => taskUpdateSelect(item.id, item.description)}
+                  />
+                </>
+              )}
+              <img
+                src="./../src/images/delete.png"
+                alt="Eliminar tareas"
+                onClick={() => onHandleDelete(item.id)}
+              />
             </div>
-            )) : "No tasks"}
-        </div>
-    );
+          ))
+        : "No tasks"}
+    </div>
+  );
 };
 
 export default Tasklist;
